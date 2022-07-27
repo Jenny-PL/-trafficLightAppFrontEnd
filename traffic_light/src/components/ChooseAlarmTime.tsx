44e72fd860e2630c2aaf7ddedfbd92c63387d21a
Jenny <jfperham@gmail.com>
2022-07-27 15:10:01 -0700

    modified ChooseAlarmTime component

--- traffic_light/src/components/ChooseAlarmTime.tsx
+++ traffic_light/src/components/ChooseAlarmTime.tsx
@@ -1,6 +1,6 @@
 import React, {useState} from "react";
 import "./ChooseAlarmTime.css";
-import { Navigate }  from 'react-router-dom';
+import { useNavigate }  from 'react-router-dom';
 
 
 // interface ChooseAlarmTimeProps {
@@ -10,21 +10,32 @@ import { Navigate }  from 'react-router-dom';
 
 const defaultTime = "00:00";
 
-// On submit, want to render this route:  <Route path="alarm" element={<BasicAlarm /> } />
 function ChooseAlarmTime(): JSX.Element {
     const [alarmTime, setAlarmTime] = useState(defaultTime);
+    const navigate = useNavigate();
 
+
+// changes alarm time state
     const onFormSubmit = (event: React.FormEvent<HTMLInputElement>) => {
         event.preventDefault();
         let newTime = event.currentTarget.value
         setAlarmTime(newTime);
-        return <Navigate  to='/alarm' />
     }
+// navigates to new page, upon submission
+// On submit, want to render this route:  <Route path="alarm" element={<BasicAlarm /> } />
+    const handleFormSubmission = (event: React.FormEvent<HTMLFormElement>) => {
+        event.preventDefault();
+        navigate('/alarm');
+    }
+
 
     return (
         <div>
             <label htmlFor="alarm">Ok to wake time: </label>
+            <form onSubmit={event => handleFormSubmission(event)}>
             <input type='time' id="alarm" name="alarm" onChange={event => onFormSubmit(event)} value={alarmTime}/>
+            <input type='submit' value='Set Alarm'></input>
+            </form>
         </div>
 
 )}
