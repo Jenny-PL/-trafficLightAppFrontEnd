d128e8190283581bf69fd8b153dad836e42d9c42
Jenny <jfperham@gmail.com>
2022-08-16 14:11:58 -0700

    fixed toggle on/off, songs move on to the next track

--- traffic_light/src/components/MusicPlay.tsx
+++ traffic_light/src/components/MusicPlay.tsx
@@ -1,25 +1,22 @@
 import "./MusicPlay.css";
-import React from 'react';
+import React, {useRef} from 'react';
 
 interface MusicPlayProps {
-    setfileURL: Function;
     songList: any;
-    fileURL: string;
 }
 
 const url = "https://traffic-light-clock-be.herokuapp.com"
 
-const MusicPlay: Function = ({setfileURL, songList, fileURL}:MusicPlayProps) => {
+const MusicPlay: Function = ({songList}:MusicPlayProps) => {
 
     let uploadedSongs = songList['songList'];
     console.log(uploadedSongs);
     let clickCount: number = 0;
-    let audio: HTMLMediaElement = document.getElementById('playlist');
 
+    const audio = useRef<HTMLAudioElement>(null);
     const [songTrack, setSongTrack] = React.useState(0)
-    let songName =  uploadedSongs[songTrack];
+    const songName =  uploadedSongs[songTrack];
     const [playSong, setPlaySong] = React.useState(`${url}/alarmsong/${songName}`)
-
 
 // toggle music on/off by clicking on music icon: 
     const toggleMusic = () => {
@@ -28,56 +25,44 @@ const MusicPlay: Function = ({setfileURL, songList, fileURL}:MusicPlayProps) => 
         console.log(clickCount);
         loadSongs(songTrack);
         if (clickCount %2 === 0) {
-            audio?.pause();}
+            audio?.current?.pause();}
         else {
-            audio?.play();
-            audio.onended = (event) => loadSongs(songTrack+1); //not happening!
-        }
+            audio?.current?.play();
+            if (audio?.current) {
+            audio.current.onended = (event) => loadSongs(songTrack+1); 
+        }}
     }
-// BUG: This load doesn't seem to run!  How to get the onEnded function to run?
+
     const loadSongs = (songTrack:any) => {
-        // songTrack += 1;
         setSongTrack(songTrack);       
         if (songTrack > uploadedSongs.length-1) {
             songTrack = 0;
+            const songName =  uploadedSongs[songTrack];
             let dbURL = `${url}/alarmsong/${songName}`;
+            if (audio?.current) {
+            audio.current.src = dbURL;
             setPlaySong(dbURL);
+            audio.current.play();}
         }
         else {
+            const songName =  uploadedSongs[songTrack];
             let dbURL = `${url}/alarmsong/${songName}`;
-            setPlaySong(dbURL);
+            if (audio?.current) {
+                audio.current.src = dbURL;
+                setPlaySong(dbURL);
+                audio.current.play();}
         }
     }
-    // const handleClick = () => {
-    //     count += 1;
-    //     let audioElement = new Audio(playSong);
-    //     audioElement?.setAttribute('autoPlay', ""); 
-    //     if (count > uploadedSongs.length-1) {
-    //         count = 0;
-    //         let dbURL = `${url}/alarmsong/${songName}`;
-    //         setPlaySong(dbURL);
-    //     }
-    //     else {
-    //         let dbURL = `${url}/alarmsong/${songName}`;
-    //         setPlaySong(dbURL);
-    //     }
-    //     audioElement.play();
-    // }
+
 
     return (
         <>
-
         <p onClick={(event) => toggleMusic()}>🎵</p>
         <p> Play music</p>
-      
-        <audio id="playlist" onEnded={(event) => loadSongs(songTrack+1)}> 
+        <audio ref= {audio} id="playlist" > 
             <source src={playSong} type="audio/mpeg"/>
             <source src={playSong} type="audio/wav"/>
         </audio>
-
-
-        {/* <div onClick={() =>audioElement.play()} >🎵</div> */}
-        {/* <div onClick={(event) => handleClick()} >🎵</div> */}
         </>
     )
   };
